7d2b1bb310a495811162712dc88353ca320a1fe5
syagawa <syagawapp@gmail.com>
2023-02-01 11:41:07 +0900

    Change install-extensions.sh

--- install-extensions.sh
+++ install-extensions.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-
-while read l
-do
-  code --install-extension $l
-done < extensions.txt
+code --install-extension amandeepmittal.pug
+code --install-extension Asuka.insertnumbers
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension Kelvin.vscode-sshfs
+code --install-extension ms-vscode-remote.remote-containers
+code --install-extension ms-vscode-remote.remote-ssh
+code --install-extension ms-vscode-remote.remote-ssh-edit
+code --install-extension ms-vscode-remote.remote-ssh-explorer
+code --install-extension ms-vscode-remote.remote-wsl
+code --install-extension ms-vscode-remote.vscode-remote-extensionpack
+code --install-extension octref.vetur
