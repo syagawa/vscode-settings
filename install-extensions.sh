dcc03b278f7d8fcf7446e3abe48494fa31c6a5c9
syagawa <syagawapp@gmail.com>
2023-09-22 10:23:07 +0900

    Update install-extensions.sh

--- install-extensions.sh
+++ install-extensions.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 code --install-extension amandeepmittal.pug
 code --install-extension Asuka.insertnumbers
+code --install-extension CraigMaslowski.erb
 code --install-extension dbaeumer.vscode-eslint
+code --install-extension humao.rest-client
 code --install-extension Kelvin.vscode-sshfs
 code --install-extension ms-vscode-remote.remote-containers
 code --install-extension ms-vscode-remote.remote-ssh
@@ -9,4 +11,5 @@ code --install-extension ms-vscode-remote.remote-ssh-edit
 code --install-extension ms-vscode-remote.remote-ssh-explorer
 code --install-extension ms-vscode-remote.remote-wsl
 code --install-extension ms-vscode-remote.vscode-remote-extensionpack
+code --install-extension Vue.volar
 code --install-extension octref.vetur
